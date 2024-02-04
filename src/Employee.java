b79c1e12bfb2ec9194b8a47e2794f362772821b8
Orhan Secilmis <66383550+Ghostfear1713@users.noreply.github.com>
2024-02-04 10:35:15 +0100

    4.1 + 4.2 + 4.3 done
    Made extra tasks

--- src/Employee.java
+++ src/Employee.java
@@ -1,15 +1,28 @@
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Employee {
     private String name;
+    private int age;
     private LocalDate birthDate;
 
     public Employee(String name) {
         this.name = name;
     }
 
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
     public Employee(String name, LocalDate birthDate) {
         this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public Employee(String name, int age, LocalDate birthDate) {
+        this.name = name;
+        this.age = age;
         this.birthDate = birthDate;
     }
 
@@ -29,11 +42,34 @@ public class Employee {
         this.birthDate = birthDate;
     }
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     @Override
     public String toString() {
-        return "Employee{" +
-                "name='" + name + '\'' +
-                ", birthDate=" + birthDate +
+        return "Employee " +
+                "name " + name + '\'' +
+                ", age = " + age +
+                ", birthDate = " + birthDate +
                 '}';
     }
+
+
+    public int calculateAge(){
+        //This will be our current date from the PC we use
+        LocalDate currentDate = LocalDate.now();
+        //This will calculate the period between birthdate and current date
+        Period period = Period.between(birthDate, currentDate);
+        //This will return us the value from that period, giving us the age
+        return period.getYears();
+    }
+
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
@@ -10,19 +12,11 @@ import java.util.stream.Collectors;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    //Task 1
     interface ArithmeticOperation {
         int perform(int a, int b);
     }
-
-    interface MyTransformingType{
-        int transform(int input);
-    }
-
-    interface MyValidationType{
-        boolean validate(int validateNumber);
-    }
-
-
 
     //Make an operator method that takes 2 numbers and an operation function
     static int operate(int a, int b, ArithmeticOperation op) {
@@ -37,6 +31,14 @@ public class Main {
     private static ArithmeticOperation power = (int a, int b) -> (int) Math.pow(a, b);
 
 
+
+    interface MyTransformingType{
+        int transform(int input);
+    }
+
+    interface MyValidationType{
+        boolean validate(int validateNumber);
+    }
     private static MyTransformingType doublingNumbers = x -> x*2;
     private static MyTransformingType multiplySameNumberWithItself = x -> x*x;
     private static MyValidationType divideableBy3 = x -> x % 3 == 0;
@@ -44,15 +46,143 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //TASK 1 TESTING
 //        System.out.println(operate(5, 20, addition));
+
+
 //        System.out.println(doublingNumbers.transform(25));
 //        System.out.println(multiplySameNumberWithItself.transform(100));
 //        System.out.println(divideableBy3.validate(5));
 //        System.out.println(checkIfNumberIsEven.validate(9));
 
 
+
+
+
+        /*//ASSIGNMENT 3.1
+        //________________
+        //TEXT: Apply the functional interfaces from the java.util.function package: Predicate, Consumer, Supplier, Function:
+        //Use Predicate to filter a list of integers, so only those divisible by 7 remain.
+
+        //We start off by making an Arraylist of Integers, called randomNumbers that will have all our numbers
+        ArrayList<Integer> randomNumbers = new ArrayList<>(Arrays.asList(
+                234, 567, 89, 432, 765, 123, 876, 321, 654, 98,
+                789, 543, 210, 876, 345, 678, 912, 456, 789, 234));
+
+        //Predicate takes an argument and returns a boolean - in this case, it will take a number and tell us, if that number will divide with the number 7
+        Predicate<Integer> isEvenWith7 = number -> number % 7 == 0;
+
+        List<Integer> filteredList = randomNumbers.stream()
+                .filter(isEvenWith7)
+                .collect(Collectors.toList());
+        System.out.println(filteredList);*/
+
+       /* //ASSIGNMENT 3.2
+        //______________
+        //TEXT: Use Supplier to create a list of Employee objects based on a list of names like Arrays.asList("John", "Jane", "Jack", "Joe", "Jill").
+
+        //Supplier is basically a factory that creates something for you without taking any arguments - First we want to make a list with all of our names and then a list that holds the objects
+
+        //List of names
+        List<String> names = Arrays.asList("John", "Jane", "Jack", "Joe", "Jill");
+        //Supplier to create Employee objects based on names
+        //<List<Employee>> is specifying the type of object that the Supplier will supply. In this case, it's a List containing Employee objects. () ->:  indicates that the lambda function takes no parameters.
+        Supplier<List<Employee>> employeeListSupplier = () -> names.stream()
+                .map(Employee::new) // map(Employee::new) is telling the stream to transform each element (each name in this case) by applying the Employee constructor to it. It creates a new Employee object for each name in the stream.
+                .collect(Collectors.toList()); // Collect them into a List
+        //This will trigger the execution of Supplier and obtain the result of a list of Employee objects
+        List<Employee> employees = employeeListSupplier.get();
+        //employees.forEach(System.out::println);
+
+        //ASSIGNMENT 3.3
+        //______________
+        //Use Consumer to print the list of Employee objects.
+        Consumer<Employee> employeeObjectsPrinted = employee -> System.out.println(employee);
+        employees.forEach(employeeObjectsPrinted);
+
+        //ASSIGNMENT 3.4
+        //______________
+        //Use Function to convert a list of Employee objects to a list of names.
+        Function<Employee, String> employeeToNameFunction = Employee::getName;
+        List<String> employeeNames = employees.stream()
+                .map(employeeToNameFunction)
+                .collect(Collectors.toList());
+        employees.forEach(System.out::println);
+
+        //ASSIGNMENT 3.5
+        //______________
+        //Use Predicate check if a given employee is older than 18.
+        Predicate<Employee> isOlderThan18 = em -> em.getAge() > 18;
+        //Creating the employee we want to check age on
+        Employee employee1 = new Employee("Johnny", 28);
+        if(isOlderThan18.test(employee1)){
+            System.out.println(employee1.getName() + " is older than 18");
+        } else {
+            System.out.println("The employee is not above 18");
+        }*/
+
+
+        /*//ASSIGNMENT 4.1 - Calculate the age of each employee based on their birthdate
+        //______________________________________________________________________________
+
+        //Firstly, we will create the employees we want to work with
+        Employee e1 = new Employee("Orhani", LocalDate.of(1995, 12, 6));
+        Employee e2 = new Employee("Ashkhaan", LocalDate.of(1990, 8, 25));
+        Employee e3 = new Employee("Lion", LocalDate.of(1982, 11, 3));
+
+        System.out.println(e1.calculateAge());
+        System.out.println(e2.calculateAge());
+        System.out.println(e3.calculateAge());*/
+
+        /*//ASSIGNMENT 4.2 - Calculate the average age of all employees
+        //___________________________________________________________
+        //We'll start off with creating a list with all of the employees we made in 4.1
+        List<Employee> listOfEmployeeAge = new ArrayList<>(Arrays.asList(
+                new Employee("Orhani", LocalDate.of(1995, 12, 6)),
+                new Employee("Ashkhaan", LocalDate.of(1990, 8, 25)),
+                new Employee("Lion", LocalDate.of(1982, 11, 3))
+        ));
+
+        double averageAge = listOfEmployeeAge.stream()
+                //Goes through each element in the list and gets the age
+                .mapToDouble(Employee::calculateAge)
+                //Finds the average of the ages in the list
+                .average()
+                //If nothing is found, it'll return 0.0
+                .orElse(0.0);
+
+        System.out.println(averageAge);*/
+
+        /*//ASSIGNMENT 4.3 - Filter and display employees who have birthdays in a specific month.
+        //_____________________________________________________________________________________
+        //We'll start off with creating a list with all of the employees we made previously in 4.1 and 4.2
+        List<Employee> listOfEmployeeAge = new ArrayList<>(Arrays.asList(
+                new Employee("Orhani", LocalDate.of(1995, 12, 6)),
+                new Employee("Ashkhaan", LocalDate.of(1990, 8, 25)),
+                new Employee("Lion", LocalDate.of(1982, 11, 3))
+        ));
+
+        //We want to specify the target month we want to find, so we'll make a variable targetMonth for December
+        int targetMonth = 12;
+        List<Employee> employeesInSpecificMonth = listOfEmployeeAge.stream()
+                .filter(employee -> employee.getBirthDate().getMonthValue() == targetMonth)
+                .collect(Collectors.toList());
+
+        if(!employeesInSpecificMonth.isEmpty()){
+            System.out.println("Employees with days in December: ");
+            employeesInSpecificMonth.forEach(employee -> System.out.println(employee.getName()));
+        } else {
+            System.out.println("There are no employees in that month");
+        }*/
+
+        
+
+
+
+        //STREAMS API - TASK 6.1
+
         //Collection of books
-        List<Book> books = new ArrayList<>(Arrays.asList(
+       /* List<Book> books = new ArrayList<>(Arrays.asList(
                 new Book("Harry Potter", "Author1", 2000, 300, 4.5),
                 new Book("The Green Lantern", "Author2", 2010, 250, 3.8),
                 new Book("Skeleton King", "Author1", 2015, 400, 4.2),
@@ -71,40 +201,8 @@ public class Main {
                 .average()
                 //If there are no ratings, orElse(0.0) is used to provide a default value of 0.0.
                 .orElse(0.0);
-        System.out.println("The average rating of all books are: " + averageRating);
+        System.out.println("The average rating of all books are: " + averageRating);*/
 
-
-
-
-        //ASSIGNMENT 3.1
-        ArrayList<Integer> randomNumbers = new ArrayList<>(Arrays.asList(
-                234, 567, 89, 432, 765, 123, 876, 321, 654, 98,
-                789, 543, 210, 876, 345, 678, 912, 456, 789, 234));
-
-        Predicate<Integer> isEvenWith7 = number -> number % 7 == 0;
-
-        List<Integer> filteredListOfNumbersThatCanBeDividedBy7 = randomNumbers.stream()
-                .filter(isEvenWith7)
-                .collect(Collectors.toList());
-        System.out.println(filteredListOfNumbersThatCanBeDividedBy7);
-
-        //ASSIGNMENT 3.2
-        //List of names
-        List<String> names = Arrays.asList("John", "Jane", "Jack", "Joe", "Jill");
-        //Supplier to create Employee objects based on names
-        Supplier<Employee> employeeSupplier = () -> new Employee(names.get(0));
-
-        //Create a list of Employee objects
-        List<Employee> employeeList = names.stream()
-                .map(name -> employeeSupplier.get())
-                .collect(Collectors.toList());
-        System.out.println(employeeList);
-
-
-        //ASSIGNMENT 4.1
-        List<LocalDate> birthDates = Arrays.asList(
-
-        );
 
 
 
